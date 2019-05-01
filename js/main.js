3450eb995fd6b1f01963343934e320f7c7326189
Yixuanhu0106 <42982272+Yixuanhu0106@users.noreply.github.com>
2019-05-01 14:20:45 -0400

    basic interaction

---
+++ js/main.js
@@ -0,0 +1,93 @@
+var myStyle = function(feature) {
+  switch (feature.properties.period) {
+    case "Archaic":
+      return {
+        color: '#646464'
+        // color: '#440154FF'
+      };
+    case "Classical":
+      return {
+        color: '#00395E'
+        // color: '#31688EFF'
+      };
+    case "Hellenistic":
+      return {
+        color: '#E0C389'
+        // color: '#35B779FF'
+      };
+    case "Roman":
+      return {
+        color: '#8B0E3A'
+        // color: '#FDE725FF'
+      };
+    default:
+      return {
+        color: '#f2f2f2'
+      };
+  }
+};
+
+layerAttractions = L.geoJSON(attractions, {
+  style: myStyle
+}).addTo(map);
+
+var sidebarOut = false; //the place of sidebar
+
+$('.opacity-container').hide();
+
+$(document).ready(function() {
+  $('.opacity-container').hide();
+  $('.desp-container').hide();
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal"
+  });
+
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+    sidebarOut = !sidebarOut;
+  });
+
+  layerAttractions.eachLayer(eachFeatureFunction);
+});
+
+$('#sldOpacity').on('change', function() {
+  $('#image-opacity').html(this.value);
+  layerHistorical.setOpacity(this.value);
+})
+
+$('#customSwitch1').change(function() { // toggle historical map
+  if ($('#customSwitch1').is(':checked')) {
+    layerHistorical.addTo(map);
+    sbsInitial.setLeftLayers(layerHistorical);
+    $('.opacity-container').fadeIn();
+    return;
+  } else {
+    map.removeLayer(layerHistorical);
+    sbsInitial.setLeftLayers(baseMap);
+    $('.opacity-container').fadeOut();
+  }
+});
+
+//When clicking features:
+var eachFeatureFunction = function(layer) {
+  layer.on('click', function(event) {
+    url1 = "img/monuments/" + layer.feature.properties.name + "/01.jpg";
+    url2 = "img/monuments/" + layer.feature.properties.name + "/02.jpg";
+    url3 = "img/monuments/" + layer.feature.properties.name + "/03.jpg";
+    $("#carousel-img-1").attr("src", url1);
+    $("#carousel-img-2").attr("src", url2);
+    $("#carousel-img-3").attr("src", url3);
+    if (!sidebarOut) {
+      $('#sidebar').toggleClass('active');
+      sidebarOut = !sidebarOut;
+    }
+    $("#despSubmenu p").text(layer.feature.properties.desp);
+    $('.desp-container a').text(layer.feature.properties.name);
+    $('.desp-container').fadeIn();
+    $('.opening-container').fadeOut();
+  });
+};
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
---
+++ js/setup.js
@@ -0,0 +1,31 @@
+var map = L.map('map', {
+  center: [37.973, 23.725],
+  zoom: 16,
+  zoomControl: false,
+  attributionControl: false
+});
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
+
+var baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/emilyhu/cjuykkzuz0fmk1fmpyfaywrdn/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZW1pbHlodSIsImEiOiJjanRraXBjYjAwMDZiNDRxbHg3cDlwbHA5In0.Z8oZamlBpJF4Sv58aC1c_A', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  ext: 'png'
+}).addTo(map)
+
+var historicalUrl = 'https://api.mapbox.com/styles/v1/emilyhu/cjuyljp711i0r1fod1rlg16ub/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZW1pbHlodSIsImEiOiJjanRraXBjYjAwMDZiNDRxbHg3cDlwbHA5In0.Z8oZamlBpJF4Sv58aC1c_A'
+
+var layerHistorical = L.tileLayer(historicalUrl, {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>'
+});
+
+var sbsInitial = L.control.sideBySide(baseMap, Esri_WorldImagery).addTo(map);
+
+L.control.zoom({
+  position: 'topright'
+}).addTo(map);
